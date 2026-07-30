32a6470400f18d273e3a9a9232ace02164f9464e
aml3arthurlima <arthurandradelima987@gmail.com>
2026-07-30 13:01:03 -0300

    atualizeichon6

--- src/contactsync/servico/Agenda.java
+++ src/contactsync/servico/Agenda.java
@@ -1,6 +1,8 @@
 package contactsync.servico;
 import contactsync.repositorio.RepositorioContato;
 import java.util.ArrayList;
+import java.util.Comparator;
+
 import contactsync.modelo.Contato;
 import contactsync.excecao.CampoObrigatorioException;
 import contactsync.excecao.ContatoDuplicadoException;
@@ -18,7 +20,7 @@ public class Agenda {
 
         contatos = new ArrayList<>();
 
-        repositorio = new ArquivoContato("contato.csv");
+        repositorio = new ArquivoContato("contato.txt");
     }
 
     public void adicionarContato(Contato contato) throws CampoObrigatorioException, ContatoDuplicadoException {
@@ -29,14 +31,31 @@ public class Agenda {
 
     }
 
-    public boolean excluirContato(String nome) throws ContatoNaoEncontradoException{
+    public boolean excluirContato(String nome) throws ContatoNaoEncontradoException {
 
+        boolean removido = false;
+
+        for (int i = 0; i < contatos.size(); i++) {
+
+            if (contatos.get(i).getNome().equalsIgnoreCase(nome)) {
+
+                contatos.remove(i);
+                removido = true;
+                break;
+            }
+        }
+
+        if (!removido) {
+            throw new ContatoNaoEncontradoException("Contato não encontrado.");
+        }
+
+        return true;
     }
 
     public ArrayList<Contato> listarContatos(){
-
+        ordenarContatos();
+        return contatos;
     }
-
     public ArrayList<Contato> pesquisarPorNome(String nome){
 
     }
@@ -47,6 +66,13 @@ public class Agenda {
 
     public Contato pesquisarPorEmail(String email){
 
+        for (Contato c : contatos) {
+            if (c.getEmail() != null && c.getEmail().equalsIgnoreCase(email)) {
+                return c;
+            }
+        }
+
+        return null;
     }
 
     public ArrayList<Contato> pesquisarPorCategoria(String categoria){
@@ -58,7 +84,7 @@ public class Agenda {
     }
 
     public void ordenarContatos(){
-
+        contatos.sort(Comparator.comparing(Contato::getNome, String.CASE_INSENSITIVE_ORDER));
     }
 
     public void marcaFavorito(String nome) {
@@ -70,13 +96,11 @@ public class Agenda {
     }
 
     public void salvarContatos() throws ArquivoException{
-
+        repositorio.salvar(contatos);
     }
 
     public void carregarContatos() throws ArquivoException{
-
+        contatos = repositorio.carregar();
     }
-
-
 
 }
