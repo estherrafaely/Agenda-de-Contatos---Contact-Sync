3d3ed3466b32aa225d55e213984b6fbae1d75d72
Esther Rafaely <estherrafaely.trb@gmail.com>
2026-07-30 17:13:30 -0300

    Apagando arquivo csv2

--- src/contactsync/repositorio/ArquivoContato.java
+++ src/contactsync/repositorio/ArquivoContato.java
@@ -25,7 +25,6 @@ public class ArquivoContato implements RepositorioContato {
 
             for (Contato contato : contatos) {
 
-
                 writer.write(contato.toCSV());
                 writer.newLine();
             }
